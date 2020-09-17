b2e2fc69c2f83de0602ea5535b2f3eef6faebfe2
Jackie Scholl <jackie.h.scholl@gmail.com>
2020-09-17 15:02:46 -0400

    mostly whitespace

--- racket_tictactoe.rkt
+++ racket_tictactoe.rkt
@@ -10,13 +10,15 @@
 (define empty-board (make-list 9 absent))
 
 (define (count-player board player)
-	(count (λ (x) (equal? x (present player))) board))
+	(count (λ (x) (equal? x (present player))) board)
+)
 
 (define lines-list (list
 					(list 0 1 2) (list 3 4 5) (list 6 7 8) ; horizontal lines
 					(list 0 3 6) (list 1 4 7) (list 2 5 8) ; vertical lines
 					(list 0 4 8) (list 2 4 6) ; diagonal lines
-					))
+					)
+)
 
 (define (test-line to-test) (match to-test
 							  [(list 'X 'X 'X) 'X]
@@ -24,45 +26,54 @@
 							  [(list a b c) absent]))
 
 (define (extract-lines board)
-	(map (λ (temp-line) (map (λ (x) (list-ref board x)) temp-line)) lines-list))
+	(map 
+		(λ (temp-line) (map (λ (x) (list-ref board x)) temp-line)) 
+		lines-list
+	)
+)
 
 (define (get-empties board)
-	(filter (λ (x) (absent? (list-ref board x))) (range 9)))
+	(filter (λ (x) (absent? (list-ref board x))) (range 9))
+)
 
 (define (count-empties board) (length (get-empties board)))
 
-(define (chunks-of n list) (sequence->list (in-slice n list)))
-
-(define (rows board)
-	(chunks-of 3 board))
-;  (list (take board 3) (take (drop board 3) 3) (drop board 6)))
-
 (define (print-board board)
+	(define (chunks-of n list) (sequence->list (in-slice n list)))
+	(define (rows board) (chunks-of 3 board))
+
 	(define (print-space p) (match p ['X "X"] ['O "O"] [nothing "_"]))
 	(define (print-row   row  ) (string-join (map print-space row)))
 	(string-join (map print-row (rows board)) "\n" #:after-last "\n")
 )
 
-(define (winner-helper board)
-	(filter-not absent? (map test-line (extract-lines board))))
 
 
 
 
 (define (get-end-state board)
+	(define (winner-helper board) (filter-not absent? (map test-line (extract-lines board))))
 	(define (check-winner board)
-		(define winner (winner-helper board))
-		(if (null? winner) absent (car winner)))
-	(define winner (check-winner board))
-	(if (not (absent? winner)) winner (if (eq? 0 (count-empties board)) 'Draw 'NotOver)))
+		(define winner1 (winner-helper board))
+		(if (null? winner1) absent (car winner1))
+	)
+	(define winner2 (check-winner board))
+	(if
+		(not (absent? winner2))
+		winner2
+		(if (eq? 0 (count-empties board)) 'Draw 'NotOver)
+	)
+)
 
 (define (get-current-turn board)
 	(if (= (modulo (count-empties board) 2) 0) 'O 'X))
 
 (define (move board index)
 	(if (present? (list-ref board index))
-	(display "err")
-	(list-set board index (get-current-turn board))))
+		(display "err")
+		(list-set board index (get-current-turn board))
+	)
+)
 
 
 (define (move-best board)
@@ -74,8 +85,9 @@
 	(match end-state
 		['Draw 0]
 		['NotOver (recursive-score)]
-		[winner (if (eq? winner player) 1 -1)])
-  )
+		[winner (if (eq? winner player) 1 -1)]
+	)
+)
 
 (define (choose-move board)
 	(argmax
@@ -103,7 +115,7 @@
 (display (print-board sample-board))
 (extract-lines sample-board)
 
-(check-winner sample-board)
+;(check-winner sample-board)
 
 (score sample-board2 'X)
 (score sample-board2 'O)
